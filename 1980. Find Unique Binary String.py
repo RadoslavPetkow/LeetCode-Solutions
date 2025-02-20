097043e472e3dfc73db9aaabc123e058431c45ca
Radoslav Petkov <156192995+RadoslavPetkow@users.noreply.github.com>
2025-02-21 00:39:19 +0200

    Create 1980. Find Unique Binary String.py

---
+++ 1980. Find Unique Binary String.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
+        return ''.join('1' if nums[i][i] == '0' else '0' for i in range(len(nums)))
